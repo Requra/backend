682fe030465f57d6fd2256cc687bfe3a0c1515b2
carolehab-dev <carolehab11@gmail.com>
2026-03-31 14:12:17 +0200

    (feat):Adding Fluent Validation

---
+++ Requra.Infrastructure/Validations/ApprovalValidator.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+using Requra.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Requra.Infrastructure.Validations
+{
+    public class ApprovalValidator : AbstractValidator<Approval>
+    {
+        public ApprovalValidator() { 
+          RuleFor(x=>x.Notes).MaximumLength(500).WithMessage("Notes can not exceed 500 characters");
+        
+        }
+    }
+}
