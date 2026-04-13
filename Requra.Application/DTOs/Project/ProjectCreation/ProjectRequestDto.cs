7de43e11532ff702443edd511f6d891a04b48904
MariamMagdy <mariammagdyhasehim@gmail.com>
2026-04-14 00:19:45 +0200

    (feat): Initial Create Of Create Project!

---
+++ Requra.Application/DTOs/Project/ProjectCreation/ProjectRequestDto.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Requra.Application.DTOs.Project.ProjectCreation
+{
+    internal class ProjectRequestDto
+    {
+    }
+}
