f896bc20dbb181d79d7dae6973f1cb7e1b37e1d6
MariamMagdy <mariammagdyhasehim@gmail.com>
2026-07-09 23:35:58 +0300

    feat:Project LookUp

---
+++ Requra.Application/DTOs/ProjectMembers/ProjectMemberDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Requra.Application.DTOs.ProjectMembers
+{
+    public class ProjectMemberDto
+    {
+        public string UserId { get; set; } = string.Empty;
+        public string Name { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string ProjectRole { get; set; } = string.Empty;
+
+        public string avatarUrl { get; set; } = string.Empty;
+    }
+}
